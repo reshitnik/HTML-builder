7d4f8103e2a8b577be750624a2602d188efd43bf
reshitnik <reshitnik@gmail.com>
2024-01-24 00:58:56 +0300

    fix bug

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,10 +1,12 @@
+const { error } = require('console');
 const fs = require('fs');
 const path = require('node:path')
 const notes = '06-build-page/index.js';
 const notesDir = path.dirname(notes);
 const notesFile = path.basename(notes);
 const myPath = path.join(notesDir, notesFile);
-// const stylePath = path.join(notesDir);
+const stylePath = path.join(notesDir, 'styles')
+console.log(path.basename(notes))
 
 
 // 1. создание папки project-dist
@@ -15,7 +17,26 @@ fs.mkdir(path.join(notesDir, 'project-dist'),
             return console.error(err);
         }
         console.log('project-dist created successfully!');
+        fs.readdir(stylePath, {withFileTypes: true}, (err, fileList) => {
+            if (!err) {
+                fileList.forEach((file) =>{
+                    fs.stat(`${file.path}/${file.name}`, (err, stats)=>{
+                        if(err){
+                            console.error('Eror css')
+                        }
+                        if (stats.isFile() && path.extname(file.name) === '.css') {
+                            readCss(path.join(stylePath, file.name))
+                        }
+                    })
+                });
+            } else {
+                console.error(err);
+                }
+            });
+
     });
+
+    // readAll(path.join(notesDir, 'assets'));
 
 // 2. создание папок функция
 function createFolders(pathWay){
@@ -29,41 +50,46 @@ function createFolders(pathWay){
         });
 }
 
+
+
 // копирование файлов в accets
 function copyFile(fromFile, toFile){
   fs.copyFile(fromFile, toFile, (err)=>{
         if(err){
-            console.error('Eror')
+            console.error(err)
         }
     })
 };
 
 // чтение папки assets
-function readAll(way){
-fs.readdir(way, {withFileTypes: true}, (err, fileList) => {
-    if (!err) {
-        fileList.forEach((file) =>{
-            fs.stat(`${file.path}/${file.name}`, (err, stats)=>{
-                if(err){
-                    console.error('Eror')
-                }
-                if (stats.isDirectory()) {
-                    console.log('dir path - ', path.join(notesDir, 'project-dist', 'assets', file.name))
-                    createFolders(path.join(notesDir, 'project-dist', 'assets', file.name))
-                    readAll(path.join(notesDir, 'assets', file.name))
-                }
-                if (stats.isFile ()) {
+function CopyAll(toDir, fromDir){
+    fs.mkdir(path.join(toDir),
+        { recursive: true },
+        (err) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('assets created successfully!');
+
+    
+    fs.readdir(fromDir, {withFileTypes: true}, (err, fileList) => {
+        if (!err) {
+            fileList.forEach(file => {
+                if (file.isDirectory()) {
+                    console.log(file.path, file.name)
+                    CopyAll(path.join(notesDir,'project-dist', 'assets', file.name), path.join(notesDir, 'assets', file.name))
+                } else {
                     copyFile(path.join(file.path, file.name), path.join(notesDir, 'project-dist', 'assets', path.basename(file.path), file.name))
                 }
             })
-        });
-    } else {
-        console.log('some Error');
-        }
-    })
-};
+        } else {
+            console.log('some Error');
+            }
+        })
+    });
+    };
 
-readAll(path.join(notesDir, 'assets'));
+CopyAll(path.join(notesDir, 'project-dist'), path.join(notesDir, 'assets'));
 
 // создание и компилирование файла style.css
 function createFiles(filePath, some = '') {
@@ -76,7 +102,6 @@ createFiles(path.join(notesDir, 'project-dist', 'style.css'));
 
 
 function readCss (way) {
-    // const arr = [];
     fs.readFile(way, 'utf-8', (err, data) => {
         if (err) {
             console.error(err)
@@ -87,69 +112,37 @@ function readCss (way) {
     })
 }
 
-const stylePath = path.join(notesDir, 'styles')
-
-fs.readdir(stylePath, {withFileTypes: true}, (err, fileList) => {
-    if (!err) {
-        fileList.forEach((file) =>{
-            fs.stat(`${file.path}/${file.name}`, (err, stats)=>{
-                if(err){
-                    console.error('Eror')
-                }
-                if (stats.isFile() && path.extname(file.name) === '.css') {
-                    readCss(path.join(stylePath, file.name))
-                }
-            })
-        });
-    } else {
-        console.error(err);
-        }
-    });
-
-// создание файла index.html и заполнение его
-createFiles(path.join(notesDir, 'project-dist', 'index.html'));
-
-fs.readFile(path.join(notesDir, 'template.html'), (err, htmlData) => {
-    if(!err) {
-        const partArr = htmlData.toString('utf-8').split('{{header}}');
-        const htmlArr = [];
-        htmlArr.push(partArr[0]);
-        htmlArr.push(partArr[1].split('{{articles}}')[0]);
-        htmlArr.push(partArr[1].split('{{articles}}')[1].split('{{footer}}')[0]);
-        htmlArr.push(partArr[1].split('{{articles}}')[1].split('{{footer}}')[1]);
-        // htmlArr.forEach((file, i )=> )
-        htmlArr.forEach((file, i )=> createFiles(path.join(notesDir, 'components', `part${i}.html`), some = file))
-    } else {
-        console.error(err)
+fs.readFile(path.join(notesDir, 'template.html'), 'utf-8', (err, data) =>{
+    if (err) {
+        console.err(err)
     }
-})
+    let text = data;
+    let header = data.match('{{header}}')[0]; 
+    let footer = data.match('{{footer}}')[0]; 
+    let articles = data.match('{{articles}}')[0]; 
 
-function addHtml(part) {
-fs.readFile(path.join(notesDir, 'components', part), 'utf-8' ,(err, data) => {
-    if (!err) {
-        fs.appendFile(path.join(notesDir, 'project-dist', 'index.html'), data, (err) => {
-            if(err) throw err;
-        });
-    } else {
-        console.error(err);
+    fs.readdir(path.join(notesDir, 'components'), (err, htmlList) => {
+        if (err) {
+            console.err(err)
         }
-    });
-};
-
-addHtml('part0.html');
-addHtml('header.html');
-addHtml('part1.html', 'part1.html');
-addHtml('articles.html');
-addHtml('part2.html', 'part2.html');
-addHtml('footer.html');
-addHtml('part3.html', 'part3.html');
-
-function deleteFile(part){
-    fs.unlink(path.join(notesDir, 'components', part), (err) => {
-        if (err) throw err;
+        htmlList.forEach(html => {
+            fs.readFile(path.join(notesDir, 'components', html), 'utf-8', (err, htmlText) => {
+                if (err) {console.err(err)} else{;
+                if (html.replace('.html','') == 'header') {
+                    text = text.replace(header, htmlText)
+                };
+                if (html.replace('.html','') == 'footer') {
+                    text = text.replace(footer, htmlText)
+                };
+                if (html.replace('.html','') == 'articles') {
+                    text = text.replace(articles, htmlText)
+                }
+                }
+                fs.writeFile(path.join(notesDir, 'project-dist', 'index.html'), text, (err) =>{
+                    if (err) {console.log(error)}
+                })
+            })
+        })
+    })
 })
-}
 
-deleteFile('part0.html');
-deleteFile('part1.html');
-deleteFile('part2.html');
